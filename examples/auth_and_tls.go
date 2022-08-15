a058957369a4824cdb01be7a6fc66a181def29ec
amunra <amunra@users.noreply.github.com>
2022-08-15 14:21:00 +0100

    chore: Added examples and manifest.

---
+++ examples/auth_and_tls.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "time"
+
+    qdb "github.com/questdb/go-questdb-client"
+)
+
+func main() {
+    ctx := context.TODO()
+    sender, err := qdb.NewLineSender(
+		ctx,
+		qdb.WithAddress("localhost:9009"),
+		qdb.WithAuth(
+			"testUser1",
+			"5UjEMuA0Pj5pjK8a-fa24dyIf-Es5mYny3oE_Wmus48"),
+		qdb.WithTls(),
+	)
+    if err != nil {
+        log.Fatal(err)
+    }
+    // Make sure to close the sender on exit to release resources.
+    defer sender.Close()
+    // Send a few ILP messages.
+    err = sender.
+        Table("trades").
+        Symbol("name", "test_ilp1").
+        Float64Column("value", 12.4).
+        At(ctx, time.Now().UnixNano())
+    if err != nil {
+        log.Fatal(err)
+    }
+    err = sender.
+        Table("trades").
+        Symbol("name", "test_ilp2").
+        Float64Column("value", 11.4).
+        At(ctx, time.Now().UnixNano())
+    if err != nil {
+        log.Fatal(err)
+    }
+    // Make sure that the messages are sent over the network.
+    err = sender.Flush(ctx)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
