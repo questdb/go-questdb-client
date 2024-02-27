4785bedd922cee2db0a7023a35bee150e8f81b19
Steven Sklar <steven@questdb.io>
2024-02-27 09:30:13 -0500

    fix

--- sender.go
+++ sender.go
@@ -1013,7 +1013,7 @@ func (s *LineSender) flushHttp(ctx context.Context) error {
 		err = fmt.Errorf("Non-OK Status Code %d: %s", resp.StatusCode, resp.Status)
 	}
 	defer resp.Body.Close()
-defer resp.Body.Close()
+
 	if s.retryTimeout > 0 {
 		retryInterval = 10 * time.Millisecond
 		for {
